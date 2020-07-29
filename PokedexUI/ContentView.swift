82e605a6dd47e5540af48bac3974467c64095f59
Bruno Vinicius <brunosiqueira@ioasys.com.br>
2020-07-29 18:42:38 -0300

    Initial Commit

---
+++ PokedexUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokedexUI
+//
+//  Created by Bruno Vinicius on 29/07/20.
+//  Copyright © 2020 Bruno Vinicius. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
